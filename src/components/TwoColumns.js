2e71aa63ca0f6db6bebf21e1d92e9ff12a9c0343
William Velazquez <william.velazquez.a@gmail.com>
2020-06-15 00:26:06 -0500

    (components): Adding Styled Components Dependency and Creating TwoColumns Component

---
+++ src/components/TwoColumns.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: grid;
+  grid-gap: 20px;
+  grid-template-columns: 1fr;
+  @media screen and (min-width: 768px) {
+    grid-template-columns: 1fr 1fr;
+  }
+`;
+
+const TwoColumns = (props) => {
+  const { children } = props;
+  return (
+    <Container>
+      {
+        children
+      }
+    </Container>
+  );
+};
+
+export default TwoColumns;
